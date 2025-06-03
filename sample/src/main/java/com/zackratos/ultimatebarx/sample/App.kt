994b19a75a9f6198969a8462be693832aae208af
chamith shaminda silva <122098794+chamithsilva10@users.noreply.github.com>
2025-06-03 10:48:10 +0530

    Update App.kt

--- sample/src/main/java/com/zackratos/ultimatebarx/sample/App.kt
+++ sample/src/main/java/com/zackratos/ultimatebarx/sample/App.kt
@@ -3,23 +3,21 @@ package com.zackratos.ultimatebarx.sample
 import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
+import androidx.appcompat.app.AppCompatDelegate
 
 /**
- * @Author   : zackratos
- * @Date     : 2021/8/23 7:58 下午
- * @email    : zhangwenchao@soulapp.cn
- * @Describe :
+ * Application class for UltimateBarX sample.
+ * 
+ * @Author zackratos
+ * @Date 2021/8/23 7:58 PM
+ * @Email zhangwenchao@soulapp.cn
  */
-class App: Application() {
+class App : Application() {
 
     companion object {
         @SuppressLint("StaticFieldLeak")
-        lateinit var context: Context
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        context = this
-    }
-
-}
+        @Volatile
+        private var _context: Context? = null
+        
+        val context: Context
+            get() = _context ?: throw IllegalStateException("Application context not initialized!")
